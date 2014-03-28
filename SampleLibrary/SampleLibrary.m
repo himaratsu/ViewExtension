5c0af1c2f21928f5d1dbe0e6ed44bdaeb50b99e5
Ryosuke Hiramatsu <himaratsu@gmail.com>
2014-03-28 11:27:09 +0900

    Initial Commit

---
+++ SampleLibrary/SampleLibrary.m
@@ -0,0 +1,13 @@
+//
+//  SampleLibrary.m
+//  SampleLibrary
+//
+//  Created by rhiramat on 2014/03/28.
+//  Copyright (c) 2014年 Ryosuke Hiramatsu. All rights reserved.
+//
+
+#import "SampleLibrary.h"
+
+@implementation SampleLibrary
+
+@end
---
+++ SampleLibraryTests/SampleLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  SampleLibraryTests.m
+//  SampleLibraryTests
+//
+//  Created by rhiramat on 2014/03/28.
+//  Copyright (c) 2014年 Ryosuke Hiramatsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SampleLibraryTests : XCTestCase
+
+@end
+
+@implementation SampleLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
